6d158d719a62e96b1391d8d094534193027e97b7
Nils Neumann <nilsneumann1999@gmail.com>
2022-10-15 19:50:02 +0200

    Initial Commit with homepage created

---
+++ tbx/sites/homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Freiwillige Feuerwehr Neulußheim</title>
+</head>
+<body>
+<div>
+    <h1>Willkommen auf der Seite der Freiwilligen Feuerwehr Neulußheim!</h1>
+    <h3>Hier finden sie immer die neusten Informationen unserer Feuerwehr</h3>
+</div>
+<div>
+    <img src="../pictures/FFN_GESAMT_1.JPG" alt="ERROR404" width="auto" height="1000">
+</div>
+</body>
+</html>
